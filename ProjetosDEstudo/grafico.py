c12db3ce6306dfd1db8e1dfb96ba8a4b2b751860
VictorWolneiH <victorwdasilva@gmail.com>
2025-12-19 14:34:43 -0300

    updt

--- ProjetosDEstudo/grafico.py
+++ ProjetosDEstudo/grafico.py
@@ -2,22 +2,33 @@ import tkinter as tk
 
 janela = tk.Tk()
 
-
-class Application():
-    def __init__(self):
-        self.janela = janela
-        self.tela()
-        janela.mainloop()
-
-    def tela(self):
-        self. janela.title("Provavelmente uma calculadora")
-        self.janela.configure(background='purple')
-        self.janela.geometry("400x320")
-        self.janela.resizable(True, True)
-        self.janela.maxsize(width=860, height=640)
-        self.janela.minsize(width=200, height=160)
+def tela():
+    janela.title("Provavelmente vai ser uma calculadora")
+    janela.configure(background='purple')
+    janela.geometry("400x320")
+    janela.resizable(True, True)
+    janela.maxsize(width=860, height=640)
+    janela.minsize(width=200, height=160)
+tela()
 
 
+texto = tk.Label(janela, text="arigato")
+texto.pack()
 
 
-Application()
+def botaor():
+    texto['text'] = "aaaaaahhhnnnn"
+
+
+
+
+
+
+
+        
+
+
+tk.Button(janela, text="me aperte", command=botaor).pack()
+
+
+janela.mainloop()
